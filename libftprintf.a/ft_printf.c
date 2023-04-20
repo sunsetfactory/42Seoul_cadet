09bfdfc2bdc30f8669e1a0c0ef0dd2dfe2bcd0a2
Seokjun Yang <seokjyan@c5r7s2.42seoul.kr>
2023-04-20 15:48:23 +0900

    20230420_1

--- libftprintf.a/ft_printf.c
+++ libftprintf.a/ft_printf.c
@@ -6,19 +6,76 @@
 /*   By: seokjyan <seokjyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 14:58:35 by seokjyan          #+#    #+#             */
-/*   Updated: 2023/04/19 23:22:07 by seokjyan         ###   ########.fr       */
+/*   Updated: 2023/04/20 15:47:54 by seokjyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+#include <unistd.h>
 
 void	pr_check_format(char *format, va_list ap);
 int		ft_printf(const char *format, ...);
+int		pr_print_pxX(unsigned long long nb, int *len, char *format);
+int		pr_print_diu(long long nb, int *len, char *format);
 
-
-void	pr_check_format(char *format, va_list ap)
+int	pr_print_pxX(unsigned long long nb, int *len, char *format)
 {
-	
+	if (nb >= 16)
+	{
+		if (pr_print_pxX(nb / 16, len, format) == -1)
+			return (-1);
+		pr_print_pxX(nb % 16, len, format);
+	}
+	else if (*format != 'X')
+	{
+		++(*len);
+		if (write(1, &"0123456789abcdef"[nb], 1) == -1)
+			return (-1);
+	}
+	else
+	{
+		++(*len);
+		if (write(1, &"0123456789ABCDEF"[nb], 1) == -1)
+			return (-1);
+	}
+	return (0);
+}
+
+int	pr_print_diu(long long nb, int *len, char *format)
+{
+	if (nb == -2147483648)
+	{
+		len += 2;
+		if (write(1, "-2", 2) == -1)
+			return (-1);
+		nb = 147483648;
+	}
+	if (nb >= 10)
+	{
+		if (pr_print_diu(nb / 10, len, format))
+			return (-1);
+		pr_print_diu(nb % 10, len, format);
+	}
+	else
+	{
+		++len;
+		if (write(1, ('0' + (unsigned int)nb), 1) == -1)
+			return (-1);
+	}
+}
+
+void	pr_check_separator(char *format, va_list ap, int *len)
+{
+	if (*format == 'x' || *format == 'X' || *format == 'p')
+	{
+		if (*format == 'p')
+			*len += write(1, "0x", 2);
+		pr_print_pxX((unsigned long long)va_arg(ap, char *), len, format);
+	}
+	if (*format == 'd' || *format == 'i' || *format == 'u')
+		pr_print_diu((long long)va_arg(ap, long long), len, format);
+	if (*format == '%')
+		*len += write(1, "%", 1);
 }
 
 int	ft_printf(const char * format, ...)
@@ -28,17 +85,29 @@ int	ft_printf(const char * format, ...)
 
 	va_start(ap, format);
 	len = 0;
-	while (*(format + len) != '\0')
+	while (*format != '\0')
 	{
 		if (*format == '%')
-			pr_check_format(format, ap);
+			pr_check_separator((char *)(++format), ap, &len);
 		else
+		{
+			++len;
 			write(1, format, 1);
-		len++;
+		}
+		++format;
 	}
+	return (len);
 }
+
+#include <stdio.h>
 
 int	main()
 {
-	ft_printf("%d, %i, %c, %s, %x, %X", 1, 2, '3', '45', 6, 7);
+	char *pp;
+	unsigned int u;
+
+	u = 4294967295;
+	printf("%u", u);
+	// printf("test1_ft_re : %d\n", ft_printf("TEST1_ft_wr : %x, %X, %p, %%\n", 31, 31, pp));
+	// printf("test1_or_re : %d\n", printf("TEST1_or_wr : %x, %X, %p, %%\n", 31, 31, pp));
 }
--- libftprintf.a/prac.c
+++ libftprintf.a/prac.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	while (*(s))
+	{
+		write(fd, s, 1);
+		s++;
+	}
+}
 
 void	printValues(char *types, ...)
 {
@@ -11,17 +21,29 @@ void	printValues(char *types, ...)
 	{
 		switch (*types)
 		{
-			case 'i':
-				printf("%d ", va_arg(ap, int));
-				break ;
-			case 'd':
-				printf("%f ", va_arg(ap, double));
-				break ;
-			case 'c':
-				printf("%c ", va_arg(ap, char));
-				break ;
+			// case 'i':
+			// 	printf("%d ", va_arg(ap, int));
+			// 	break ;
+			// case 'd':
+			// 	printf("%f ", va_arg(ap, double));
+			// 	break ;
+			// case 'c':
+			// 	printf("%c ", va_arg(ap, int));
+			// 	break ;
+			// case 's':
+			// 	printf("%s ", va_arg(ap, char *));
+			// 	break ;
+			// case 'p':
+			// 	printf("%p ", va_arg(ap, char *));
+			// 	break ;
+			// default:
+			// 	break ;
 			case 's':
-				printf("%s ", va_arg(ap, char *));
+				ft_putstr_fd(va_arg(ap, char *), 1);
+				break ;
+			case 'p':
+				write(1, "0x", 2);
+				ft_get_hex((unsigned long long)va_arg(ap, char *));
 				break ;
 			default:
 				break ;
@@ -34,8 +56,12 @@ void	printValues(char *types, ...)
 
 int main()
 {
-	printValues("i", 10);
-	printValues("ci", 'a', 10);
-	printValues("dci", 1.234567, 'a', 10);
-	printValues("sicd", "Hello, world!", 10, 'a', 1.234567);
+	char *pp;
+
+	// printValues("i", 10);
+	// printValues("ci", 'a', 10);
+	// printValues("dci", 1.234567, 'a', 10);
+	// printValues("sicd", "Hello, world!", 10, 'a', 1.234567);
+	printValues("psicd", pp, "Hello, world!", 10, 'a', 1.234567);
+	printf("\n%p", pp);
 }
