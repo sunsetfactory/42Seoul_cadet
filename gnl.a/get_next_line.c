170ecd94ca63b60de1c890ab75ccbbbf2042e83f
Seokjun Yang <seokjyan@c5r1s6.42seoul.kr>
2023-07-06 03:05:24 +0900

    20230706_1

--- gnl.a/get_next_line.c
+++ gnl.a/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: seokjyan <seokjyan@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 10:03:09 by seokjyan          #+#    #+#             */
-/*   Updated: 2023/07/05 23:51:12 by seokjyan         ###   ########.fr       */
+/*   Updated: 2023/07/06 02:58:23 by seokjyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ char	*stat_cut(char *buf_stat, int i)
 	char	*cut_stat;
 	int		size;
 
+	char *temp = buf_stat;
 	buf_stat += i;
 	size = ft_strlen(buf_stat);
 	cut_stat = (char *)malloc(sizeof(char) * (size + 1));
@@ -26,6 +27,7 @@ char	*stat_cut(char *buf_stat, int i)
 		*(cut_stat + i) = *(buf_stat + i);
 		++i;
 	}
+	free(temp);
 	return (cut_stat);
 }
 
@@ -35,11 +37,13 @@ char	*line_get(char *buf_stat, int *i)
 	char	*ret_strr;
 	char	*cut_line;
 
-	ret_strr = ft_strrchr(buf_stat, '\n');
+	if (*buf_stat == '\0')
+		return (NULL);
+	ret_strr = ft_strchr(buf_stat, '\n'); 
 	if (ret_strr == NULL)
 		size = ft_strlen(buf_stat);
 	else
-		size = ret_strr - buf_stat;
+		size = ret_strr - buf_stat + 1;
 	cut_line = (char *)malloc(sizeof(char) * (size + 1));
 	if (!cut_line)
 		return (NULL);
@@ -55,30 +59,27 @@ char	*line_get(char *buf_stat, int *i)
 
 char	*buf_memory_allocation(int fd, char *buf_stat)
 {
-	char	*buf_read;
+	char	buf_read[BUFFER_SIZE + 1];
 	int		ret_read;
 	char	*tmp_stat;
 
-	buf_read = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buf_read)
-		return (NULL);
 	if (buf_stat == NULL)
 		buf_stat = ft_strdup("");
 	ret_read = 1;
-	while (ft_strrchr(buf_stat, '\n') == NULL && ret_read != 0)
+	while (ft_strchr(buf_stat, '\n') == NULL && ret_read != 0)
 	{
 		ret_read = read(fd, buf_read, BUFFER_SIZE);
 		if (ret_read == -1)
 		{
-			free(buf_read);
+			// free(buf_stat); // TODO
 			return (NULL);
 		}
 		buf_read[ret_read] = '\0';
 		tmp_stat = buf_stat;
-		free(buf_stat);
+		// free(buf_stat);
 		buf_stat = ft_strjoin(tmp_stat, buf_read);
+		free(tmp_stat);
 	}
-	free(buf_read);
 	return (buf_stat);
 }
 
@@ -90,31 +91,68 @@ char	*get_next_line(int fd)
 
 	if (fd < 0)
 		return (NULL);
-	if (ft_strrchr(buf_stat, '\n') == NULL)
+	if (ft_strchr(buf_stat, '\n') == NULL)
 		buf_stat = buf_memory_allocation(fd, buf_stat);
 	cut_line = line_get(buf_stat, &i);
-	buf_stat = stat_cut(buf_stat, i);
+	if (cut_line == NULL)
+	{
+		free(buf_stat);
+		buf_stat = 0;
+	}
+	else
+		buf_stat = stat_cut(buf_stat, i);
 	return (cut_line);
 }
 
-int	main(void)
+void	check_leak(void)
 {
-	int		fd;
-	char	*line;
-
-	fd = 0;
-	fd = open("./input.txt", O_RDONLY);
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	// while ((line = get_next_line(fd)) != NULL)
-	// {
-	// 	printf("%s", line);
-	// 	free(line);
-	// }
-	if (line == NULL)
-		printf("%s\n", line);
-	close(fd);
-	return (0);
+	system("leaks --list -- a.out");
 }
+
+// #include <stdlib.h>
+
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*line;
+
+// 	fd = 0;
+// 	fd = open("./input.txt", O_RDONLY);
+// 	// printf("%s", get_next_line(fd));
+// 	// system("leaks --list -- a.out");
+// 	// printf("%s", get_next_line(fd));
+// 	// system("leaks --list -- a.out");
+// 	// printf("%s", get_next_line(fd));
+// 	// system("leaks --list -- a.out");
+// 	// printf("%s", get_next_line(fd));
+// 	// system("leaks --list -- a.out");
+// 	// printf("%s", get_next_line(fd));
+// 	// system("leaks --list -- a.out");
+// 	// printf("%s", get_next_line(fd));
+// 	// system("leaks --list -- a.out");
+// 	while ((line = get_next_line(fd)) != NULL)
+// 	{
+// 		atexit(check_leak);
+// 		printf("%s", line);
+// 		free(line);
+// 	}
+// 	if (line == NULL)
+// 		printf("%s\n", line);
+// 	close(fd);
+// 	return (0);
+// }
+
+// #include <stdio.h>
+
+// int main() {
+// 	int fd = open("empty.txt", O_RDONLY);
+// 	char *str;
+// 	while ((str = get_next_line(fd)) != NULL)
+// 	{
+// 		printf("%s", str);
+// 		free(str);
+// 	}
+// 	close(fd);
+// 	system("leaks a.out");
+// 	return 0;
+// }
--- gnl.a/get_next_line_utils.c
+++ gnl.a/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: seokjyan <seokjyan@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 10:03:07 by seokjyan          #+#    #+#             */
-/*   Updated: 2023/07/05 23:55:08 by seokjyan         ###   ########.fr       */
+/*   Updated: 2023/07/06 02:35:35 by seokjyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,45 +57,60 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	return (dst);
 }
 
-char	*ft_strjoin(char const	*s1, char const	*s2)
-{
-	size_t	s1_len;
-	size_t	s2_len;
-	char	*res;
+// char	*ft_strjoin(char const	*s1, char const	*s2)
+// {
+// 	size_t	s1_len;
+// 	size_t	s2_len;
+// 	char	*res;
 
-	if (!s1 || !s2)
+// 	if (!s1 || !s2)
+// 		return (NULL);
+// 	s1_len = ft_strlen(s1);
+// 	s2_len = ft_strlen(s2);
+// 	res = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+// 	if (!res)
+// 		return (NULL);
+// 	ft_memcpy(res, s1, s1_len);
+// 	ft_memcpy(res + s1_len, s2, s2_len);
+// 	res[s1_len + s2_len] = 0;
+// 	printf("s1: %p, res: %p\n", s1, res);
+// 	return (res);
+// }
+
+char *ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (s1 == NULL && s2 == NULL)
+		return (0);
+	else if (s1 == NULL)
+		return (ft_strdup(s2));
+	else if (s2 == NULL)
+		return (ft_strdup(s1));
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == 0)
 		return (NULL);
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	res = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
-	if (!res)
-		return (NULL);
-	ft_memcpy(res, s1, s1_len);
-	ft_memcpy(res + s1_len, s2, s2_len);
-	res[s1_len + s2_len] = 0;
-	return (res);
+	while (s1[i] != '\0')
+		str[j++] = s1[i++];
+	i = 0;
+	while (s2[i] != '\0')
+		str[j++] = s2[i++];
+	str[j] = '\0';
+	// free(s1);
+	return (str);
 }
 
-char	*ft_strrchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-	char	*pos;
-
 	if (s == NULL)
 		return (NULL);
-	pos = 0;
-	if ((char)c == '\0')
-	{
-		while (*s)
-			s++;
-		pos = (char *)s;
-	}
-	while (*s)
-	{
-		if (*s == (char)c)
-			pos = (char *)s;
+	while (*s && *s != (char)c)
 		s++;
-	}
-	if (*pos == '\0')
-		pos = NULL;
-	return (pos);
+	if (*s == '\0' && (char)c != '\0')
+		return (NULL);
+	return ((char *)s);
 }
