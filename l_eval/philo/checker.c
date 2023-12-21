68563d09c766b6e84fffefcdd015531186e31058
Seokjun Yang <seokjyan@cx1r5s7.42seoul.kr>
2023-12-21 14:40:29 +0900

    20231221_2

---
+++ l_eval/philo/checker.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seokjyan <seokjyan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 00:09:05 by seokjyan          #+#    #+#             */
+/*   Updated: 2023/12/20 00:09:05 by seokjyan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	*checker_death(void *data)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)data;
+	while (1)
+	{
+		pthread_mutex_lock(&philo->eat_mtx);
+		if (philo->table->state == RUN
+			&& get_present_time() > philo->death_time)
+		{
+			pthread_mutex_unlock(&philo->eat_mtx);
+			msg_print(END_STARV, philo);
+			pthread_mutex_lock(&philo->eat_mtx);
+			pthread_mutex_unlock(&philo->table->main_mtx);
+			return (NULL);
+		}
+		pthread_mutex_unlock(&philo->eat_mtx);
+		usleep(1000);
+	}
+}
+
+void	*checker_cnt(void *data)
+{
+	t_table	*table;
+	int		i;
+	int		cnt;
+
+	table = (t_table *)data;
+	i = 0;
+	cnt = 0;
+	while (1)
+	{
+		if (table->philo_arr[i].cnt_eat >= table->num_must_eat && \
+		table->philo_arr[i].all_ate == 0)
+		{
+			pthread_mutex_lock(&table->philo_arr[i].eat_mtx);
+			table->philo_arr[i].all_ate = 1;
+			++cnt;
+		}
+		if (cnt >= table->num_philo)
+			break ;
+		if (++i >= table->num_philo)
+			i = 0;
+	}
+	msg_print(END_COUNT, &table->philo_arr[0]);
+	pthread_mutex_lock(&table->msg_mtx);
+	pthread_mutex_unlock(&table->main_mtx);
+	return (NULL);
+}
---
+++ l_eval/philo/deallocate.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   deallocate.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seokjyan <seokjyan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 00:09:08 by seokjyan          #+#    #+#             */
+/*   Updated: 2023/12/20 00:09:08 by seokjyan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	free_fork_m(t_table *table)
+{
+	int	i;
+
+	i = 0;
+	if (table->fork_mtx_arr)
+	{
+		while (i < table->num_philo)
+			pthread_mutex_destroy(&table->fork_mtx_arr[i++]);
+		free(table->fork_mtx_arr);
+	}
+}
+
+void	free_eat_m(t_table *table)
+{
+	int	i;
+
+	i = 0;
+	if (table->philo_arr)
+	{
+		while (i < table->num_philo)
+			pthread_mutex_destroy(&table->philo_arr[i++].eat_mtx);
+		free(table->philo_arr);
+	}
+}
+
+int	deallocate_all(t_table *table)
+{
+	free_fork_m(table);
+	free_eat_m(table);
+	pthread_mutex_destroy(&table->main_mtx);
+	pthread_mutex_destroy(&table->msg_mtx);
+	return (1);
+}
