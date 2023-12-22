7dbce871cc4a4534ced92e05f2a50109a0dd22a2
Seokjun Yang <seokjyan@c5r6s1.42seoul.kr>
2023-12-22 22:42:36 +0900

    20231222_0

---
+++ philo_ver_1/philo/init.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seokjyan <seokjyan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 00:09:13 by seokjyan          #+#    #+#             */
+/*   Updated: 2023/12/20 00:09:13 by seokjyan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./philo.h"
+
+int	init_philo_arr(t_table *table)
+{
+	int		i;
+	t_philo	*philo;
+
+	i = 0;
+	table->philo_arr = (t_philo *)malloc(sizeof(t_philo) * table->num_philo);
+	if (!table->philo_arr)
+		return (ERR);
+	while (i < table->num_philo)
+	{
+		philo = &(table->philo_arr[i]);
+		philo->all_ate = 0;
+		philo->index = i + 1;
+		philo->table = table;
+		philo->lfork = &(table->fork_mtx_arr[i]);
+		philo->rfork = &(table->fork_mtx_arr[(i + 1) % table->num_philo]);
+		philo->lfork_idx = i;
+		philo->rfork_idx = (i + 1) % table->num_philo;
+		philo->cnt_eat = 0;
+		if (pthread_mutex_init(&table->philo_arr[i].eat_mtx, NULL))
+			return (ERR);
+		i++;
+	}
+	return (0);
+}
+
+int	init_fork_mtx_arr(t_table *table)
+{
+	int	i;
+
+	table->fork_mtx_arr = (pthread_mutex_t *)malloc
+		(sizeof(pthread_mutex_t) * table->num_philo);
+	if (!table->fork_mtx_arr)
+		return (ERR);
+	i = 0;
+	while (i < table->num_philo)
+	{
+		if (pthread_mutex_init(&table->fork_mtx_arr[i], NULL))
+			return (ERR);
+		i++;
+	}
+	return (0);
+}
+
+int	init_table(t_table *table)
+{
+	pthread_mutex_lock(&table->state_mtx);
+	table->state = RUN;
+	pthread_mutex_unlock(&table->state_mtx);
+	if (init_fork_mtx_arr(table) == ERR)
+		return (ERR);
+	if (init_philo_arr(table) == ERR)
+		return (ERR);
+	if (pthread_mutex_init(&table->msg_mtx, NULL))
+		return (ERR);
+	if (pthread_mutex_init(&table->main_mtx, NULL))
+		return (ERR);
+	if (pthread_mutex_init(&table->state_mtx, NULL))
+		return (ERR);
+	if (pthread_mutex_init(&table->time_die_mtx, NULL))
+		return (ERR);
+	if (pthread_mutex_init(&table->time_eat_mtx, NULL))
+		return (ERR);
+	if (pthread_mutex_init(&table->time_sleep_mtx, NULL))
+		return (ERR);
+	if (pthread_mutex_init(&table->start_time_mtx, NULL))
+		return (ERR);
+	pthread_mutex_lock(&table->main_mtx);
+	return (0);
+}
---
+++ philo_ver_1/philo/routine.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seokjyan <seokjyan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 00:09:30 by seokjyan          #+#    #+#             */
+/*   Updated: 2023/12/20 00:09:30 by seokjyan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./philo.h"
+
+void	take_fork(t_philo *philo)
+{
+	pthread_mutex_lock(philo->lfork);
+	msg_print(L_FORK, philo);
+	pthread_mutex_lock(philo->rfork);
+	msg_print(R_FORK, philo);
+}
+
+void	put_fork(t_philo *philo)
+{
+	pthread_mutex_unlock(philo->lfork);
+	pthread_mutex_unlock(philo->rfork);
+}
+
+void	eating(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->eat_mtx);
+	philo->cnt_eat++;
+	msg_print(EAT, philo);
+	philo->last_eat_time = get_present_time();
+	philo->death_time = get_present_time() + philo->table->time_die;
+	pthread_mutex_unlock(&philo->eat_mtx);
+	pthread_mutex_lock(&philo->table->time_eat_mtx);
+	ft_usleep(philo->table->time_eat);
+	pthread_mutex_unlock(&philo->table->time_eat_mtx);
+}
+
+int	sleep_and_think(t_philo *philo)
+{
+	msg_print(SLEEP, philo);
+	ft_usleep(philo->table->time_sleep);
+	msg_print(THINK, philo);
+	return (0);
+}
+
+void	*routine(void *data)
+{
+	t_philo		*philo;
+	// pthread_t	trd;
+
+	philo = (t_philo *)data;
+	philo->last_eat_time = get_present_time();
+	philo->death_time = get_present_time() + philo->table->time_die;
+	// if (pthread_create(&trd, NULL, &checker_death, data))
+	// {
+	// 	err_msg(ERR_MAIN4);
+	// 	return (NULL);
+	// }
+	// pthread_detach(trd);
+	while (1)
+	{
+		take_fork(philo);
+		eating(philo);
+		put_fork(philo);
+		sleep_and_think(philo);
+	}
+	return (NULL);
+}
