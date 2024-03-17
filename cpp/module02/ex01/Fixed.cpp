2a24252ba47354c97c2de8f2e8255e2c3ab66c8a
sunsetfactory <yang1194us@gmail.com>
2024-03-17 17:07:03 +0900

    Automated commit at Sun Mar 17 17:07:01 KST 2024

---
+++ cpp/module02/ex01/Fixed.cpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seokjyan <seokjyan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/15 22:32:19 by seokjyan          #+#    #+#             */
+/*   Updated: 2024/03/17 17:05:36 by seokjyan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : fixedPointValue(0)
+{
+    std::cout << "Default constructor called" << std::endl; 
+}
+
+Fixed::Fixed(const int value)
+{
+    std::cout << "Int constructor called" << std::endl;
+    fixedPointValue = value << fractionalBits;
+}
+
+Fixed::Fixed(const float value)
+{
+    std::cout << "Float constructor called" << std::endl;
+    fixedPointValue = (int)roundf(value * (1 << fractionalBits));
+}
+
+
+Fixed::Fixed(const Fixed &fixed)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = fixed;
+}
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed)
+{
+    std::cout << "Assignation operator called" << std::endl;
+    fixedPointValue = fixed.getRawBits();
+    return *this;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return fixedPointValue;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    fixedPointValue = raw;
+}
+
+float Fixed::toFloat( void ) const
+{
+    return (float)fixedPointValue / (1 << fractionalBits);
+}
+
+int Fixed::toInt( void ) const
+{
+    return fixedPointValue >> fractionalBits;
+}
---
+++ cpp/module02/ex01/Fixed.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seokjyan <seokjyan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/15 22:32:24 by seokjyan          #+#    #+#             */
+/*   Updated: 2024/03/17 17:06:55 by seokjyan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+    private:
+        int					fixedPointValue;
+        static const int	fractionalBits = 8;
+    public:
+        Fixed();
+        Fixed(const int value);
+        Fixed(const float value);
+        Fixed(const Fixed &fixed);
+        ~Fixed();
+        Fixed &operator=(const Fixed &fixed);
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
+        float toFloat( void ) const;
+        int toInt( void ) const;
+};
+
+#endif
