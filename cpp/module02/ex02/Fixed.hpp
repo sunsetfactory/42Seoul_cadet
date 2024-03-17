7bb08158dd7ce46ba7de721f4e2bf32f413e393e
sunsetfactory <yang1194us@gmail.com>
2024-03-17 18:08:00 +0900

    Automated commit at Sun Mar 17 18:07:58 KST 2024

--- cpp/module02/ex02/Fixed.hpp
+++ cpp/module02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: seokjyan <seokjyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 22:32:24 by seokjyan          #+#    #+#             */
-/*   Updated: 2024/03/17 17:51:39 by seokjyan         ###   ########.fr       */
+/*   Updated: 2024/03/17 18:04:49 by seokjyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,8 +43,7 @@ class Fixed
         Fixed &operator=(const Fixed &fixed);
         Fixed &operator++();
         Fixed operator++(int);
-        Fixed &operator--();
-        Fixed operator--(int);
+        Fixed ();
         Fixed operator+(const Fixed &fixed) const;
         Fixed operator-(const Fixed &fixed) const;
         Fixed operator*(const Fixed &fixed) const;
