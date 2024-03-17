2f4266d97e86b05a46429dc3787000b329e18442
sunsetfactory <yang1194us@gmail.com>
2024-03-17 21:44:14 +0900

    Automated commit at Sun Mar 17 21:44:13 KST 2024

--- cpp/module02/ex02/main.cpp
+++ cpp/module02/ex02/main.cpp
@@ -6,29 +6,39 @@
 /*   By: seokjyan <seokjyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 22:32:21 by seokjyan          #+#    #+#             */
-/*   Updated: 2024/03/17 21:39:10 by seokjyan         ###   ########.fr       */
+/*   Updated: 2024/03/17 21:44:13 by seokjyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+#define cout std::cout
 
 int main( void ) {
   Fixed a;
   Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
-  std::cout << "a = " << a << std::endl;
-  std::cout << "b = " << b << std::endl;
-  std::cout << Fixed::max( a, b ) << std::endl;
-  std::cout << a + b << std::endl;
-  std::cout << a - b << std::endl;
-  std::cout << a * b << std::endl;
-  std::cout << a / b << std::endl;
+  cout << "a = " << a << endl;
+  cout << "b = " << b << endl;
+  cout << Fixed::max( a, b ) << endl;
+  cout << a + b << endl;
+  cout << a - b << endl;
+  cout << a * b << endl;
+  cout << a / b << endl;
   
-  std::cout << "a++, a: ";
-  std::cout.width(10);  std::cout << a++ << ", ";  std::cout.width(10);  std::cout << a << std::endl;
-  std::cout << "++a, a: ";
-  std::cout.width(10);  std::cout << ++a << ", ";
-  std::cout.width(10);  std::cout << a << std::endl;
+  cout << "a++, a: ";
+  cout.width(10);  cout << a++ << ", ";  cout.width(10);  cout << a << endl;
+  cout << "++a, a: ";
+  cout.width(10);  cout << ++a << ", ";  cout.width(10);  cout << a << endl;
 
+  cout << "a = " << a << endl;
+  cout << "b = " << b << endl;
+
+  Fixed c(2);
+  Fixed d(2);
+  Fixed e(3);
+
+  if (c < d)
+    cout << d << endl;
+  
   return 0;
 }
