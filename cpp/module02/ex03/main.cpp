7560696b535d92a69ad98e7de69a7c6f4045ab77
seokjyan <sieun@seokjyan.local>
2024-04-12 19:58:27 +0900

    Automated commit at 2024년 4월 12일 금요일 19시 58분 27초 KST

--- cpp/module02/ex03/main.cpp
+++ cpp/module02/ex03/main.cpp
@@ -16,12 +16,12 @@
 
 int main(void)
 {
-    // Point a(8, 4);
-    // Point b(0, 4);
-    // Point c(6, 2);
-    Point a(10, 10);
-    Point b(6, 6);
-    Point c(12, 4);
+    Point a(8, 4);
+    Point b(0, 4);
+    Point c(6, 2);
+    // Point a(10, 10);
+    // Point b(6, 6);
+    // Point c(12, 4);
     Point point(10.0f, 6.0f);
 
     std::cout << "point: " << point.getX() << ", " << point.getY() << std::endl;
