29fb800c4cedc5b49444768c889c36f1b6b04f83
seokjyan <sieun@seokjyan.local>
2024-04-12 19:04:13 +0900

    Automated commit at 2024년 4월 12일 금요일 19시 04분 13초 KST

--- cpp/module02/ex03/Point.cpp
+++ cpp/module02/ex03/Point.cpp
@@ -49,5 +49,6 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
     Fixed const a1 = (x - x1) * (y2 - y1) - (x2 - x1) * (y - y1);
     Fixed const a2 = (x - x2) * (y3 - y2) - (x3 - x2) * (y - y2);
     Fixed const a3 = (x - x3) * (y1 - y3) - (x1 - x3) * (y - y3);
+
     return (a1 >= 0 && a2 >= 0 && a3 >= 0) || (a1 <= 0 && a2 <= 0 && a3 <= 0);
 }
