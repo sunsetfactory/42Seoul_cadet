f81592945039a3ced0ded25afb2460ef12673b31
seokjyan <sieun@seokjyan.local>
2024-04-12 14:03:08 +0900

    Automated commit at 2024년 4월 12일 금요일 14시 03분 08초 KST

--- cpp/module02/ex02/main.cpp
+++ cpp/module02/ex02/main.cpp
@@ -34,13 +34,17 @@ int main(void)
     Fixed const b(Fixed(5.05f) * Fixed(2));
     std::cout << std::endl;
 
+    std::cout << "a = " << a << ", b = " << b << std::endl << std::endl;
+
     std::cout << a << std::endl;
     std::cout << ++a << std::endl;
     std::cout << a << std::endl << std::endl;
+
     std::cout << a++ << std::endl;
     std::cout << a << std::endl;
-    std::cout << b << std::endl << std::endl;
-    std::cout << Fixed::max(a, b) << std::endl;
+    std::cout << a << std::endl << std::endl;
+
+    std::cout << Fixed::max(a, b) << std::endl << std::endl;
     return 0;
 
     // cout << "a++, a: ";
