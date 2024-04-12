45ed2577fd17cf1d348c62c6620ffdf0b2313ad0
seokjyan <sieun@seokjyan.local>
2024-04-12 14:48:22 +0900

    Automated commit at 2024년 4월 12일 금요일 14시 48분 22초 KST

--- cpp/module02/ex02/main.cpp
+++ cpp/module02/ex02/main.cpp
@@ -33,14 +33,43 @@ int main(void)
 
     std::cout << Fixed::max(a, b) << std::endl << std::endl;
 
-    std::cout << "/*================test2================*/" << std::endl;
+    std::cout << std::endl << "/*================test2================*/" << std::endl;
     Fixed c(42.42f);
     Fixed d(21.21f);
-    std::cout << Fixed::max(c, d) << std::endl << std::endl;
+    std::cout << Fixed::max(c, d) << std::endl;
 
-    std::cout << "/*================test3================*/" << std::endl;
+    std::cout << std::endl << "/*================test3================*/" << std::endl;
     Fixed const e(42.42f);
     Fixed const f(21.21f);
-    std::cout << Fixed::min(e, f) << std::endl << std::endl;
+    std::cout << Fixed::min(e, f) << std::endl;
+
+    std::cout << std::endl << "/*================test4================*/" << std::endl;
+    Fixed g(42.42f);
+    Fixed h(21.21f);
+    if (g > h)
+        std::cout << "g is greater than h" << std::endl;
+    else
+        std::cout << "g is less than or equal to h" << std::endl;
+
+    std::cout << std::endl << "/*================test5================*/" << std::endl;
+    Fixed i(42.42f);
+    Fixed j(42.42f);
+
+    if (i == j)
+        std::cout << "i is equal to j" << std::endl;
+    else
+        std::cout << "i is not equal to j" << std::endl;
+
+    std::cout << std::endl << "/*================test6================*/" << std::endl;
+
+    Fixed k(42.42f);
+    Fixed l(10.10f);
+    std::cout << k + l << std::endl;
+    std::cout << k - l << std::endl;
+    std::cout << k * l << std::endl;
+    std::cout << k / l << std::endl;
+
+    std::cout << std::endl << "/*================destructor call================*/" << std::endl;
+
     return 0;
 }
