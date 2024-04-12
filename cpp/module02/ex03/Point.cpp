d90173a25355c904df6c5be92caeb688a40dec80
seokjyan <sieun@seokjyan.local>
2024-04-12 19:09:15 +0900

    Automated commit at 2024년 4월 12일 금요일 19시 09분 15초 KST

--- cpp/module02/ex03/Point.cpp
+++ cpp/module02/ex03/Point.cpp
@@ -40,15 +40,15 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
     Fixed const x = point.getX();
     Fixed const y = point.getY();
-    Fixed const x1 = a.getX();
-    Fixed const y1 = a.getY();
-    Fixed const x2 = b.getX();
-    Fixed const y2 = b.getY();
-    Fixed const x3 = c.getX();
-    Fixed const y3 = c.getY();
-    Fixed const a1 = (x - x1) * (y2 - y1) - (x2 - x1) * (y - y1);
-    Fixed const a2 = (x - x2) * (y3 - y2) - (x3 - x2) * (y - y2);
-    Fixed const a3 = (x - x3) * (y1 - y3) - (x1 - x3) * (y - y3);
+    Fixed const a_x = a.getX();
+    Fixed const a_y = a.getY();
+    Fixed const b_x = b.getX();
+    Fixed const b_y = b.getY();
+    Fixed const c_x = c.getX();
+    Fixed const c_y = c.getY();
+    Fixed const a1 = (x - a_x) * (b_y - a_y) - (b_x - a_x) * (y - a_y);
+    Fixed const a2 = (x - b_x) * (c_y - b_y) - (c_x - b_x) * (y - b_y);
+    Fixed const a3 = (x - c_x) * (a_y - c_y) - (a_x - c_x) * (y - c_y);
 
     return (a1 >= 0 && a2 >= 0 && a3 >= 0) || (a1 <= 0 && a2 <= 0 && a3 <= 0);
 }
