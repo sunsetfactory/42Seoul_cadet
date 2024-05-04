7adbe7550210d47c297806e819f5bacebdda55a6
seokjyan <yang1194us@gmail.com>
2024-05-04 21:30:33 +0900

    modify_ex00

--- cpp/module06/ex00/ScalarConverter.hpp
+++ cpp/module06/ex00/ScalarConverter.hpp
@@ -3,25 +3,35 @@
 
 #include <cctype>  // std::isprint
 #include <iomanip> // std::setprecision
+#include <iosfwd>  // std::stringstream
 #include <iostream>
 #include <limits> // std::numeric_limits
 
 class ScalarConverter
 {
   private:
-    // unused private member functions
+    // unused private member functions, but required by the subject
     ScalarConverter();
     ScalarConverter(const ScalarConverter &obj);
     ~ScalarConverter();
     ScalarConverter &operator=(const ScalarConverter &obj);
 
+    // private member functions
     static void convertChar(const std::string &str);
     static void convertInt(const std::string &str);
     static void convertFloat(const std::string &str);
     static void convertDouble(const std::string &str);
+    static void convertScalar(const std::string &str);
+
+    // extra private member functions
+    static void convertInf(const std::string &str);
+    static void convertNan(const std::string &str);
+    static bool isInf(const std::string &str);
+    static bool isNan(const std::string &str);
 
   public:
+    // only static member functions
     static void convert(const std::string &str);
 };
 
-#endif // SCALARCONVERTER_HPP
+#endif
