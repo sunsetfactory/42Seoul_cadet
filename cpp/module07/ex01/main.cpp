e07d257db1a810c320bd3fc44e50dab48c086c4f
seokjyan <yang1194us@gmail.com>
2024-05-10 17:40:55 +0900

    finish_module07

--- cpp/module07/ex01/main.cpp
+++ cpp/module07/ex01/main.cpp
@@ -1,21 +1,20 @@
 #include "iter.hpp"
 
-template <typename T> void print(T const &x)
-{
-    std::cout << x << std::endl;
+template <typename T>
+void print(T const &x) {
+  std::cout << x << std::endl;
 }
 
-int main(void)
-{
-    int intArray[] = {1, 2, 3, 4, 5};
-    float floatArray[] = {1.1f, 2.2f, 3.3f, 4.4f, 5.5f};
-    std::string stringArray[] = {"one", "two", "three", "four", "five"};
+int main(void) {
+  int intArray[] = {1, 2, 3, 4, 5};
+  float floatArray[] = {1.1f, 2.2f, 3.3f, 4.4f, 5.5f};
+  std::string stringArray[] = {"one", "two", "three", "four", "five"};
 
-    std::cout << "Int array:" << std::endl;
-    iter(intArray, 5, print);
-    std::cout << "Float array:" << std::endl;
-    iter(floatArray, 5, print);
-    std::cout << "String array:" << std::endl;
-    iter(stringArray, 5, print);
-    return 0;
+  std::cout << "Int array:" << std::endl;
+  iter(intArray, 5, print);
+  std::cout << "Float array:" << std::endl;
+  iter(floatArray, 5, print);
+  std::cout << "String array:" << std::endl;
+  iter(stringArray, 5, print);
+  return 0;
 }
