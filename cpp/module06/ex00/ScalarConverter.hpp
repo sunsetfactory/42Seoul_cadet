62f0556cba75709f1fabdcc5f11b2b9e5ceb49d5
seokjyan <yang1194us@gmail.com>
2024-05-28 14:12:31 +0900

    eval_module_06

--- cpp/module06/ex00/ScalarConverter.hpp
+++ cpp/module06/ex00/ScalarConverter.hpp
@@ -5,33 +5,34 @@
 #include <iomanip> // std::setprecision
 #include <iosfwd>  // std::stringstream
 #include <iostream>
-#include <limits> // std::numeric_limits
+#include <limits>  // std::numeric_limits
+#include <cstdlib> // std::strtod
 
 class ScalarConverter
 {
-  private:
-    // unused private member functions, but required by the subject
-    ScalarConverter();
-    ScalarConverter(const ScalarConverter &obj);
-    ~ScalarConverter();
-    ScalarConverter &operator=(const ScalarConverter &obj);
+private:
+  // unused private member functions, but required by the subject
+  ScalarConverter();
+  ScalarConverter(const ScalarConverter &obj);
+  ~ScalarConverter();
+  ScalarConverter &operator=(const ScalarConverter &obj);
 
-    // private member functions
-    static void convertChar(const std::string &str);
-    static void convertInt(const std::string &str);
-    static void convertFloat(const std::string &str);
-    static void convertDouble(const std::string &str);
-    static void convertScalar(const std::string &str);
+  // private member functions
+  static void convertChar(const std::string &str);
+  static void convertInt(const std::string &str);
+  static void convertFloat(const std::string &str);
+  static void convertDouble(const std::string &str);
+  static void convertScalar(const std::string &str);
 
-    // extra private member functions
-    static void convertInf(const std::string &str);
-    static void convertNan(const std::string &str);
-    static bool isInf(const std::string &str);
-    static bool isNan(const std::string &str);
+  // extra private member functions
+  static void convertInf(const std::string &str);
+  static void convertNan(const std::string &str);
+  static bool isInf(const std::string &str);
+  static bool isNan(const std::string &str);
 
-  public:
-    // only static member functions
-    static void convert(const std::string &str);
+public:
+  // only static member functions
+  static void convert(const std::string &str);
 };
 
 #endif
