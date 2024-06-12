52ccab1f6961fed7a219f9325d72154f49718691
Seokjun Yang <seokjyan@c3r2s1.42seoul.kr>
2024-06-12 15:08:22 +0900

    eval_module08

--- cpp/module08/ex00/easyfind.hpp
+++ cpp/module08/ex00/easyfind.hpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <stdexcept>
+#include <algorithm>
 
 template <typename T>
 typename T::iterator easyfind(T &container, int value)
 {
-  typename T::iterator it =
-      std::find(container.begin(), container.end(), value);
+  typename T::iterator it = std::find(container.begin(), container.end(), value);
   if (it == container.end())
   {
     throw std::runtime_error("Value not found in container");
@@ -16,8 +16,7 @@ typename T::iterator easyfind(T &container, int value)
 template <typename T>
 typename T::iterator easyfind(const T &container, int value)
 {
-  typename T::iterator it =
-      std::find(container.begin(), container.end(), value);
+  typename T::iterator it = std::find(container.begin(), container.end(), value);
   if (it == container.end())
   {
     throw std::runtime_error("Value not found in container");
