c2962fae896827a2e58e6ab066e079e8da186c35
seokjyan <yang1194us@gmail.com>
2024-06-21 16:40:04 +0900

    Automated commit at 2024년 6월 21일 금요일 16시 40분 04초 KST

--- cpp/module09/ex01/RPN.cpp
+++ cpp/module09/ex01/RPN.cpp
@@ -49,8 +49,10 @@ void	RPN::calculate(std::string expr)
 			continue;
 		else if (isdigit(expr[i]))
 			_stack.push(static_cast<int>(expr[i] - 48));
+		// 연산자인 경우
 		else if (RPN::isop(expr[i]))
 		{
+			// 스택에 2개 이상의 숫자가 있을 때만 연산을 수행
 			if (_stack.size() >= 2)
 				RPN::perform_operation(_stack, expr[i]);
 			else
@@ -58,6 +60,7 @@ void	RPN::calculate(std::string expr)
 		}
 		else
 			print_error();
+		std::cout << _stack.top() << std::endl;
 	}
 	if (_stack.size() == 1)
 		std::cout << _stack.top() << std::endl;
--- cpp/module09/ex01/main.cpp
+++ cpp/module09/ex01/main.cpp
@@ -13,7 +13,8 @@
 #include "RPN.hpp"
 
 int	main(int ac, char **av) {
-	if (ac != 2) {
+	if (ac != 2)
+	{
 		std::cout << "Error" << std::endl;
 		return -1;
 	}
