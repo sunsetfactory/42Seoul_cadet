c6f3a159135c715c1557ca0faa42bffc68c12cdc
seokjyan <yang1194us@gmail.com>
2024-06-21 13:15:40 +0900

    Automated commit at 2024년 6월 21일 금요일 13시 15분 40초 KST

--- cpp/module04/ex03/AMateria.hpp
+++ cpp/module04/ex03/AMateria.hpp
@@ -5,6 +5,8 @@
 
 
 // 전방선언이란? 전방선언은 컴파일러에게 이 변수는 나중에 정의될 것이라고
+//*정의(definition)는 식별자(identifier)를 실제로 구현하거나 인스턴스화(메모리 할당) 한다. *
+//*선언(declaration)은 식별자를 소개하고, 식별자가 나중에 정의될 것이라고 약속한다. *
 class ICharacter;
 
 class AMateria
