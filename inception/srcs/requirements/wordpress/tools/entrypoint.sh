160188c7e810f3418cbffc218f76657d06375164
seokjyan <yang1194us@gmail.com>
2024-09-02 14:10:01 +0900

    update inception

--- inception/srcs/requirements/wordpress/tools/entrypoint.sh
+++ inception/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
-# Start MariaDB
-exec mysqld --user=mysql --console
+wget -O /tmp/wordpress.tar.gz "http://wordpress.org/latest.tar.gz"
+tar -xvzf /tmp/wordpress.tar.gz -C /var/www/html
